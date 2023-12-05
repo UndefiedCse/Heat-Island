203761002319d6248193f88cb3ec90b7e64e7944
Lori Won <loriwon@uw.edu>
2023-12-05 12:51:43 -0800

    Updating first page Updated imports Updated how to access other folks' codes

--- heat_island/heat_island_main.py
+++ heat_island/heat_island_main.py
@@ -5,10 +5,12 @@ Starting python page for Heat Island. User will run this page to get acess to th
 import folium
 import time
 
-from heat_island.getcoor import getcoor
+from getcoor.getcoor import select_coordinate
+from data_process import input_file_from_data_dir
 
 cities = {"seattle": (47.606, -122.333)}
 existing = False
+# boundaryPath = 'data/seattle-city-limits.geojson'
 
 # If users want to input another set of data (new city)
 print("We currently have data for the following city/cities:")
@@ -17,7 +19,7 @@ response = input("What city would you like to view? Type 'New' for a new city.")
 
 while existing == False:
     if response.lower() in cities :
-        city = response
+        city = response.lower()
         # How to get access to the info needed for using Seattle map + Weather?
         existing = True
     elif response.lower() == 'new':
@@ -25,20 +27,26 @@ while existing == False:
         city = input("What is the name of the new city?")
         coordNew = input("What is the latitude/longitude coordinates of this city?")
         cities[city] = coordNew
-        # DO SOMETHING - I'm not sure exactly what information we would need
-        # 1. Request weather data, in specific format
-        # 2. Request city LIDAR data, in specific format
-        # 4. Need to verify the existing model works for this new set of data. If not, 
-        #     let user know that current model doesn't work; will need to retrain? 
+
+        # Requests required data for new model formation/new height info        
+        print("Move your weather data into the 'data' directory, using the following format: 'city_weather'. e.g. 'seattle_weather'.")
+        filler = input("Press any key to continue")
+        print("Move your city boundary into the 'data' directory, using the following format: 'city_boundary'. e.g. 'seattle_boundary'.")
+        filler = input("Press any key to continue")
+        print("Move your building data into the 'data' directory, using the following format: 'city_building'. e.g. 'seattle_building'.")
+        filler = input("Press any key to continue")
+
         existing = True
     else:
         print("This is not a valid response. Please either type a city name or 'New'.")
         response = input("What city would you like to view? Type 'New' for a new city.")
 
+#input_file_from_data_dir(file name)
 print("Please display both this page, and the following map simultaneously.")
 time.sleep(2)  #Pauses to allow readers to read the message above
 # Possibly loop, starting from here.
 # Call functions from getcoor.py here! Returns latitude/longitude coordinates.
+select_coordiate(boundaryPath)
 
 # Call the ML model here?
 # Call functions for applying ML here
