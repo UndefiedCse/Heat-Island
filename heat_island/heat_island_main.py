fa171320a813565fb483ae3e35b7dc676ea3ba2e
Lori Won <loriwon@uw.edu>
2023-12-07 12:56:06 -0800

    Updated style

--- heat_island/heat_island_main.py
+++ heat_island/heat_island_main.py
@@ -2,15 +2,14 @@
 Starting python page for Heat Island. User will run this page to get acess to the maps, etc.
 '''
 
-import folium
 import time
 
-from getcoor.getcoor import select_coordinate
-from data_process import input_file_from_data_dir
-from geo_process import create_hexagon
+from heat_island.getcoor.getcoor import select_coordinate
+from heat_island.data_process import input_file_from_data_dir
+from heat_island.geo_process import create_hexagon
 
 cities = {"seattle": (47.606, -122.333)}
-existing = False
+EXISTING = False
 # boundaryPath = 'data/seattle-city-limits.geojson'
 
 # If users want to input another set of data (new city)
@@ -18,27 +17,30 @@ print("We currently have data for the following city/cities:")
 print(*cities)
 response = input("What city would you like to view? Type 'New' for a new city.")
 
-while existing == False:
+while not EXISTING:
     if response.lower() in cities :
         city = response.lower()
         # How to get access to the info needed for using Seattle map + Weather?
-        existing = True
+        EXISTING = True
     elif response.lower() == 'new':
         # 3. Request city name, and append to above list, cities
         city = input("What is the name of the new city?").lower()
         coordNew = input("What is the latitude/longitude coordinates of this city?")
         cities[city] = coordNew
 
-        # Requests required data for new model formation/new height info        
-        print("Move your weather data into the 'data' directory, using the following format: 'city_weather'. e.g. 'seattle_weather'.")
+        # Requests required data for new model formation/new height info       
+        print('''Move your weather data into the 'data' directory, using the following
+        format: 'city_weather'. e.g. 'seattle_weather'.''')
         filler = input("Press any key to continue")
-        print("Move your city boundary into the 'data' directory, using the following format: 'city_boundary'. e.g. 'seattle_boundary'.")
+        print('''Move your city boundary into the 'data' directory, using the following 
+        format: 'city_boundary'. e.g. 'seattle_boundary'.''')
         filler = input("Press any key to continue")
-        print("Move your building data into the 'data' directory, using the following format: 'city_building'. e.g. 'seattle_building'.")
+        print('''Move your building data into the 'data' directory, using the following 
+        format: 'city_building'. e.g. 'seattle_building'.''')
         filler = input("Press any key to continue")
 
         radius = input("Please set the radius used for ML Training. Enter 0 for default.")
-        existing = True
+        EXISTING = True
     else:
         print("This is not a valid response. Please either type a city name or 'New'.")
         response = input("What city would you like to view? Type 'New' for a new city.")
@@ -53,8 +55,8 @@ weatherFileDir = input_file_from_data_dir(city + "_weather")
 boundaryFileDir = input_file_from_data_dir(city + "_boundary")
 buildingFileDir = input_file_from_data_dir(city + "_building")
 
-morePoints = True
-while morePoints:
+MOREPOINTS = True
+while MOREPOINTS:
     # Returns latitude/longitude coordinates.
     print("Please select the coordinate where you want to run the weather model.")
     x, y = select_coordinate(boundaryFileDir)
@@ -65,7 +67,8 @@ while morePoints:
     # Call the ML model here?
 
     # Call functions for applying ML here
-    print("Please hold on as the algorithm computes the expected temperature within the region.")
+    print('''Please hold on as the algorithm computes
+    the expected temperature within the region.''')
 
     # Display data - What will it look like? Will create a folium pop-up regardless; can be a graph
     # or (various heights with various temperature predictions), or just a line of text as popup
@@ -75,9 +78,9 @@ while morePoints:
     more = input("Would you like to test more points? (y/n)")
     for i in range(3):
         if more.lower() == "n":
-            morePoints = False
+            MOREPOINTS = False
             break
-        elif more.lower() == "y":
+        if more.lower() == "y":
             break
         else:
             if i == 2:
