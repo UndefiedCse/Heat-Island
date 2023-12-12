1e09916ef43111b0a723dbea2bfe10ddfb64a5e6
Lilac H <hongzhtt@gmail.com>
2023-12-11 18:44:46 -0800

    Update geo_process.py with docstring

--- heat_island/geo_process.py
+++ heat_island/geo_process.py
@@ -1,14 +1,13 @@
-# import pandas as pd
-# import geopandas as gpd
+import pandas as pd
+import geopandas as gpd
 import shapely.geometry
 from shapely.geometry import Polygon
 # from geojson import Feature, Point, FeatureCollection
 # import json
 import math
-# import mercantile
+#import mercantile
 # from tqdm import tqdm
 # from folium import GeoJson
-# import os
 # import tempfile
 # import fiona
 import folium
@@ -19,9 +18,30 @@ import folium
 # import matplotlib
 # import matplotlib.pyplot as plt
 
-def create_hexagon(latitude, longitude):
+from data_process import input_file_from_data_dir
+
+def create_hexagon(latitude, longitude, radius_meters = 111111 * 0.001):
     """
-    
+    Creates a hexagon centered at a specified latitude and longitude.
+
+    This function generates the vertices of a regular hexagon centered 
+    at the given latitude and longitude. 
+    The hexagon is approximated on the Earth's surface, considering the
+    Earth as a sphere with a radius of 6,371,000 meters. The radius of 
+    the hexagon is set as default to 0.001 degrees of latitude, 
+    converted into meters. Each side of the hexagon is equal in length,
+    and the vertices are calculated using the central angle for a 
+    hexagon (60 degrees).
+
+    Parameters:
+    latitude (float): The latitude of the center of the hexagon.
+    longitude (float): The longitude of the center of the hexagon.
+    radius_meters (float): The desired radius of the hexagon, in meter.
+
+    Returns:
+    shapely.geometry.Polygon: A Polygon object representing the hexagon.
+    Each vertex of the hexagon is a tuple containing the latitude and 
+    longitude in decimal degrees.
     """
 
     # Earth's radius in meters
@@ -29,8 +49,7 @@ def create_hexagon(latitude, longitude):
 
     # Convert radius from degrees in latitude to meters
     # 1 degree of latitude is approximately 111,111 meters
-    # We use 0.001 degree of latitude as radius
-    radius_meters = 111111 * 0.001
+    # We use 0.001 degree of latitude as radius as default
     # Calculate the radius of our hexagon in radians
     r_rad = radius_meters / earth_radius
 
@@ -53,8 +72,27 @@ def create_hexagon(latitude, longitude):
 
     return Polygon(hexagon_vertices)
 
+
 def hex_to_geojson(hexagon):
+    """
+    Converts a hexagon shape into GeoJSON format using Folium.
+
+    This function takes a hexagon shape, typically created using a 
+    geometry library like Shapely, and converts it into a GeoJSON 
+    object using Folium. The GeoJSON object created includes type as
+    'Feature', and the geometry is derived from the input hexagon shape. 
+
+    Parameters:
+    hexagon (shapely.geometry.Polygon): A Polygon object representing 
+    the hexagon. The hexagon should be defined with its vertices as 
+    latitude and longitude pairs.
+
+    Returns:
+    folium.features.GeoJson: A GeoJson object representing the hexagon.
+    """
+
     return folium.GeoJson({
         "type": "Feature",
         "geometry": shapely.geometry.mapping(hexagon)
     })
+
