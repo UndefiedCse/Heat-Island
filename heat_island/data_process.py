3eb6e7bd053c435ebf6fc44c7d97a8a3b8feb4aa
Lilac H <hongzhtt@gmail.com>
2023-12-13 16:21:21 -0800

    modify according to pylint

--- heat_island/data_process.py
+++ heat_island/data_process.py
@@ -6,6 +6,9 @@ Functions:
         inside the 'data' directory and output input file path.
     preprocess_csv(input_file_name): Drop the rows contains missing 
         temprature data and the duplicates weather station.
+
+Author: @LilacHo
+Date: 2023/12/13
 """
 
 
@@ -68,7 +71,7 @@ def preprocess_csv(input_file_name):
     as the input file.
 
     Returns:
-    DF: The absolute path of the file within the 'data' directory.
+    d_f: The absolute path of the file within the 'data' directory.
 
     Example usage:
     preprocess_csv('weather_Seattle.csv')
@@ -78,29 +81,29 @@ def preprocess_csv(input_file_name):
     input_file_path = input_file_from_data_dir(input_file_name)
 
     # Load the CSV file into a dataframe
-    DF = pd.read_csv(input_file_path)
+    d_f = pd.read_csv(input_file_path)
     # Debug print statement - can be removed in production
-    print(DF) # print original dataframe
+    print(d_f) # print original dataframe
 
     # Drop the rows where temprature is missing
-    DF = DF.dropna(subset=['Ave temp annual_F'])
+    d_f = d_f.dropna(subset=['Ave temp annual_F'])
 
     # Drop duplicates based on the 'Station ID' column
     # This will keep only the first occurrence of each unique ID
-    DF = DF.drop_duplicates(subset=['Station ID'])
+    d_f = d_f.drop_duplicates(subset=['Station ID'])
 
     # Drop the 'Note' column
-    DF = DF.drop(columns=['Note'])
+    d_f = d_f.drop(columns=['Note'])
     # Debug print statement - can be removed in production
-    print(DF) # print the modified dataframe
+    print(d_f) # print the modified dataframe
 
     # Construct the output file path to save in the same directory as input file
     output_file_name = 'processed_' + input_file_name
     output_file_path = os.path.join(os.path.dirname(input_file_path), output_file_name)
 
     # Save the processed DataFrame back to CSV
-    DF.to_csv(output_file_path, index=False)
+    d_f.to_csv(output_file_path, index=False)
 
     print(f"Processed file saved as: {output_file_path}")
 
-    return DF
+    return d_f
